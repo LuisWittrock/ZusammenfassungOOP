ad919979b1284a9c3149274fc70e6fddefabea8c
Luis Wittrock <luiswittrock@icloud.com>
2023-02-14 12:44:20 +0100

    sdf

--- VererbungBSP.java
+++ VererbungBSP.java
@@ -1,54 +1,63 @@
-public class Animal {
+public class Animal 
+{
   private String name;
   private int age;
   private int weight;
  
-  public void setName(String name) {
+  public void setName(String name) 
+  {
     this.name = name;
   }
-  public void setAge(int age) {
+  public void setAge(int age) 
+  {
     this.age = age;
   }
-  public void setWeight(int weight) {
+  public void setWeight(int weight) 
+  {
     this.weight = weight;
   } 
 
-  public void kaka() {
+  public void kaka() 
+  {
     System.out.println("kaka");
     this.weight -= 3;
   }
  
 }
 
-public class Dog extends Animal {
-  
-
-  public void bark() {
+public class Dog extends Animal 
+{
+  public void bark() 
+  {
     System.out.println("wuff");
   }
 }
 
-public class Cat extends Animal {
-
-
+public class Cat extends Animal 
+{
   public void purr() {
     System.out.println("purr");
   }
 }
 
-public class LittleDog extends Dog {
-
+public class LittleDog extends Dog 
+{
   @Override //ueberschreibt die bark methode von der Dog klasse kann aber auch auf alle methoden der Animal klasse zugreifen.
   public void bark() {
     System.out.println("nervigeres wuff");
   }
 }
 
-public class Main {
-
+public class Main 
+{
   public static void main(String[] args) {
     Dog dog = new Dog();
-    dog.setName("Hund");
+
+    dog.setName("Doggo");    
+
+    dog.bark();
+    dog.kaka();
+    dog.setAge(3);
   }
 }
  
